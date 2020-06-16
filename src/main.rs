f44b9949cc7ab954ded39ce9cbeecea9603ca3c8
Chris Ha <chunghha@users.noreply.github.com>
2020-06-16 03:54:13 -0500

    chore: customized rustfmt

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use tide::log;
 mod fibonacci;
 
 fn main() -> Result<()> {
-    log::start();
+  log::start();
 
-    task::block_on(async {
-        let mut app = tide::new();
-        app.at("/fib/:n").get(fibonacci::get_fibonacci);
-        app.listen("0.0.0.0:8080").await?;
-        Ok(())
-    })
+  task::block_on(async {
+    let mut app = tide::new();
+    app.at("/fib/:n").get(fibonacci::get_fibonacci);
+    app.listen("0.0.0.0:8080").await?;
+    Ok(())
+  })
 }
