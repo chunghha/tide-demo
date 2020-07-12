e963edad20b3af29971e3408bad1e798d23335a8
Chris Ha <chunghha@users.noreply.github.com>
2020-07-12 10:52:03 -0500

    refactor: mod fibonacci with traits

--- src/fibonacci/mod.rs
+++ src/fibonacci/mod.rs
@@ -9,23 +9,41 @@ struct Fibonacci {
   duration: Duration,
 }
 
-pub async fn get_fibonacci(req: Request<()>) -> tide::Result<Response> {
-  let n: usize = req.param("n").unwrap_or(0);
-  let start = Instant::now();
-  let fib_n = fib(n);
-  let duration = Instant::now() - start;
-
-  let fib = Fibonacci { number: n, fibonacci: fib_n, duration };
-
-  let mut res = Response::new(200);
-  res.set_body(Body::from_json(&fib)?);
-  Ok(res)
+trait IFibonacci {
+  fn generate(n: usize) -> Fibonacci;
 }
 
-fn fib(n: usize) -> usize {
-  match n {
-    0 => 0,
-    1 => 1,
-    _ => fib(n - 1) + fib(n - 2),
+impl IFibonacci for Fibonacci {
+  fn generate(n: usize) -> Fibonacci {
+    let start = Instant::now();
+    let fib = Fib::fib(n);
+    let duration = Instant::now() - start;
+
+    Fibonacci { number: n, fibonacci: fib, duration }
   }
+}
+
+struct Fib {}
+
+trait IFib {
+  fn fib(n: usize) -> usize;
+}
+
+impl IFib for Fib {
+  fn fib(n: usize) -> usize {
+    match n {
+      0 => 0,
+      1 => 1,
+      _ => Fib::fib(n - 1) + Fib::fib(n - 2),
+    }
+  }
+}
+
+pub async fn get_fibonacci(req: Request<()>) -> tide::Result<Response> {
+  let n: usize = req.param("n").unwrap_or(0);
+
+  let mut res = Response::new(200);
+  res.set_body(Body::from_json(&Fibonacci::generate(n))?);
+
+  Ok(res)
 }
