2f8de61e74ec3d95320061dbdfba3f93510c8dfe
Chris Ha <chunghha@users.noreply.github.com>
2020-07-19 23:31:26 -0500

    refactor: main as async fn

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 use anyhow::Result;
-use async_std::task;
-use tide::log;
 
 mod fibonacci;
 
-fn main() -> Result<()> {
-  log::start();
+#[async_std::main]
+async fn main() -> Result<()> {
+  tide::log::start();
 
-  task::block_on(async {
-    let mut app = tide::new();
-    app.at("/fib/:n").get(fibonacci::get_fibonacci);
-    app.listen("0.0.0.0:8080").await?;
-    Ok(())
-  })
+  let mut app = tide::new();
+  app.at("/fib/:n").get(fibonacci::get_fibonacci);
+  app.listen("0.0.0.0:8080").await?;
+  Ok(())
 }
